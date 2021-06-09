80011f86e681a79f2cf986eb94c7c3480c06b0bb
rahmiyatulhasanah <74273223+rahmiyatulhasanah@users.noreply.github.com>
2021-06-09 17:57:34 +0700

    Add files via upload

---
+++ FoodOrdering/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace FoodOrdering
+{
+    partial class frm_front
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_order = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_order
+            // 
+            this.btn_order.BackColor = System.Drawing.Color.Transparent;
+            this.btn_order.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btn_order.FlatAppearance.BorderSize = 2;
+            this.btn_order.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_order.Font = new System.Drawing.Font("Montserrat SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_order.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_order.Location = new System.Drawing.Point(329, 347);
+            this.btn_order.Name = "btn_order";
+            this.btn_order.Size = new System.Drawing.Size(174, 35);
+            this.btn_order.TabIndex = 2;
+            this.btn_order.Text = "Pesan";
+            this.btn_order.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btn_order.UseVisualStyleBackColor = false;
+            this.btn_order.Click += new System.EventHandler(this.btn_order_click);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.BackColor = System.Drawing.Color.Transparent;
+            this.btn_exit.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btn_exit.FlatAppearance.BorderSize = 0;
+            this.btn_exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_exit.Font = new System.Drawing.Font("Montserrat", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_exit.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_exit.Location = new System.Drawing.Point(383, 388);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(66, 30);
+            this.btn_exit.TabIndex = 3;
+            this.btn_exit.Text = "Keluar";
+            this.btn_exit.UseVisualStyleBackColor = false;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::FoodOrdering.Properties.Resources.lOGO;
+            this.pictureBox1.Location = new System.Drawing.Point(170, 49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(482, 218);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Montserrat", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(287, 292);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(271, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Selamat Datang! ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // frm_front
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(64)))), ((int)(((byte)(88)))));
+            this.ClientSize = new System.Drawing.Size(810, 470);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_order);
+            this.Controls.Add(this.label1);
+            this.Name = "frm_front";
+            this.Text = "Home";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_order;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FoodOrdering/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FoodOrdering
+{
+    public partial class frm_front : Form
+    {
+        public frm_front()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_order_click(object sender, EventArgs e)
+        {
+            Form item = new frm_item();
+            item.Show();
+            this.Hide(); 
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            DialogResult iExit;
+            iExit = MessageBox.Show("Yakin ngga jadi mesan?", "Teras Kartini",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (iExit == DialogResult.Yes) { Application.Exit(); }
+        }
+    }
+}
